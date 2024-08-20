b06d44c46eea2c282ab424d2dfac7b5306fa8125
giuliof <giulio@glgprograms.it>
2024-08-20 23:57:56 +0200

    feat: added key override for multimedia

--- keyboards/monterey_k101/keymaps/default/keymap.c
+++ keyboards/monterey_k101/keymaps/default/keymap.c
@@ -3,6 +3,12 @@
 
 #include QMK_KEYBOARD_H
 
+#define _BASE 0
+
+#include "process_key_override.h"
+#include "quantum.h"
+#include "quantum_keycodes.h"
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /*
      * ┌───┐   ┌───┬───┬───┬───┐ ┌───┬───┬───┬───┐ ┌───┬───┬───┬───┐ ┌───┬───┬───┐
@@ -20,13 +26,31 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * │Ctrl│GUI │Alt │                        │ Alt│  \ │Menu│Ctrl│ │ ← │ ↓ │ → │ │   0   │ . │   │
      * └────┴────┴────┴────────────────────────┴────┴────┴────┴────┘ └───┴───┴───┘ └───────┴───┴───┘
      */
-    [0] = LAYOUT_asian102(
+    [_BASE] = LAYOUT_asian102(
         KC_ESC,           KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,     KC_PSCR, KC_SCRL, KC_PAUS,
 
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,    KC_INS,  KC_HOME, KC_PGUP,    KC_NUM,  KC_PSLS, KC_PAST, KC_PMNS,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,             KC_DEL,  KC_END,  KC_PGDN,    KC_P7,   KC_P8,   KC_P9,
-        KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,           KC_ENT,                                  KC_P4,   KC_P5,   KC_P6,   KC_PPLS,
+        KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,                                   KC_P4,   KC_P5,   KC_P6,   KC_PPLS,
         KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT,             KC_UP,               KC_P1,   KC_P2,   KC_P3,   KC_PENT,
         KC_LCTL, KC_LGUI, KC_LALT,                                KC_SPC,                                  KC_RALT, KC_BSLS, KC_RCTL,    KC_LEFT, KC_DOWN, KC_RGHT,    KC_P0,            KC_PDOT
     )
+};
+
+const key_override_t MPRV_key_override = ko_make_basic(MOD_BIT(KC_RALT), KC_F6, KC_MPRV);
+const key_override_t MPLY_key_override = ko_make_basic(MOD_BIT(KC_RALT), KC_F7, KC_MPLY);
+const key_override_t MNXT_key_override = ko_make_basic(MOD_BIT(KC_RALT), KC_F8, KC_MNXT);
+const key_override_t MUTE_key_override = ko_make_basic(MOD_BIT(KC_RALT), KC_F9, KC_MUTE);
+const key_override_t VOLD_key_override = ko_make_basic(MOD_BIT(KC_RALT), KC_F10, KC_VOLD);
+const key_override_t VOLU_key_override = ko_make_basic(MOD_BIT(KC_RALT), KC_F11, KC_VOLU);
+
+// This globally defines all key overrides to be used
+const key_override_t **key_overrides = (const key_override_t *[]){
+    &MPRV_key_override,
+    &MPLY_key_override,
+    &MNXT_key_override,
+    &MUTE_key_override,
+    &VOLD_key_override,
+    &VOLU_key_override,
+	NULL // Null terminate the array of overrides!
 };
